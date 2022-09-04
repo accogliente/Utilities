6a4112740d29782be21ee541c193af4835191284
accogliente <accogliente2@gmail.com>
2022-09-04 23:55:23 +0300

    add test message

---
+++ src/main/java/me/accogliente/utilities/Utilities.java
@@ -0,0 +1,19 @@
+package me.accogliente.utilities;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Utilities extends JavaPlugin {
+
+
+
+    @Override
+    public void onEnable() {
+        getServer().getConsoleSender().sendMessage("TEST");
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
